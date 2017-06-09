f6aa0de96b7e508f41a2882554c0fc1814dcfbdf
dylan coleman <dylanscoleman@gmail.com>
2017-06-09 09:07:55 -0400

    final version for presentation

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ function loadBoard() {
 
     for (var i = 0; i < 6; i++) {
         for (var j = 0; j < 7; j++) {
-            $("#gameboard").append('<div id="' + i + j + '"class="tile"></div>');
+            $("#gameboard").append('<div id="' + i + j + '"class="tile" ></div>');
         }
 
     }
@@ -35,9 +35,9 @@ function switchTurns() {
     }
 }
 
-for (var i = 0; i < tiles.length; i++) {
+ for (var i = 0; i < tiles.length; i++) {
     tiles[i].addEventListener('click', boxClickHandler)
-}
+  }
 
 var bb = [
     [0, 0, 0, 0, 0, 0, 0],
@@ -48,43 +48,46 @@ var bb = [
     [0, 0, 0, 0, 0, 0, 0]
 ];
 var color = [
-    ["w", "w", "w", "w"],
-    ["w", "w", "w", "w"],
-    ["w", "w", "w", "w"],
-    ["w", "w", "w", "w"],
-    ["w", "w", "w", "w"],
-    ["w", "w", "w", "w"],
+    ["w", "w", "w", "w","w","w"],
+    ["w", "w", "w", "w","w","w"],
+    ["w", "w", "w", "w","w","w"],
+    ["w", "w", "w", "w","w","w"],
+    ["w", "w", "w", "w","w","w"],
+    ["w", "w", "w", "w","w","w"]
 ];
 // what happens when a box is clicked
 function boxClickHandler(event) {
     //console.log('the box has been clicked.')
     //console.log(event.srcElement.id);
+    // this will give us the id of the circle eg: 00, 01, ... 56
     var boardId = event.srcElement.id;
 
-    var firstId = boardId.substring(0, 1);
-    var lastId = boardId.substring(1, 2);
-    console.log(lastId);
+    // this will give the first index of the id : eg 0 , or 1, or 5
+    var row = boardId.substring(0, 1);
+    // this will give us the second index of the id , eg: 1, 2, .. 6,
+    var column = boardId.substring(1, 2);
+
 
     // Any last row circle is allowed to click first
-    if (firstId == 5) {
-        bb[firstId][lastId] = 1;
+    if (row == 5) {
+        bb[row][column] = 1;
     }
 
-    // Check if the below box is empty? or not filled yet
-    if (firstId != 5 && bb[Number(firstId) + 1][Number(lastId)] !== 1) {
+    // Check if the below box is clicked or not or have the value =0? or not filled yet
+    if (row != 5 && bb[Number(row) + 1][Number(column)] !== 1) {
         return;
     }
 
 
     if (game.currentPlayer == game.player1) {
         $(this).addClass('red');
-        bb[firstId][lastId] = 1;
-        color[firstId][lastId] = "r";
+        bb[row][column] = 1;
+        color[row][column] = "r";
         checkRedIsWinner();
     } else if (game.currentPlayer == game.player2) {
         $(this).addClass('black');
-        bb[firstId][lastId] = 1;
-        color[firstId][lastId] = "b";
+        bb[row][column] = 1;
+        color[row][column] = "b";
         checkBlackIsWinner();
     }
     this.removeEventListener('click', boxClickHandler)
@@ -99,8 +102,8 @@ function checkRedIsWinner() {
     //Vertical Winner Check
     for (var i = 0; i < 7; i++) {
         var verticalCount = 0;
-        for (var k = 0; k < 6; k++) {
-            if (color[k][i] == "r") {
+        for (var j = 0; j < 6; j++) {
+            if (color[j][i] == "r") {
                 verticalCount++;
             } else {
                 verticalCount = 0;
@@ -135,7 +138,8 @@ function checkRedIsWinner() {
     }
 
     // Daigonal Checking for winner in all possible cases starting first column
-
+    // here we start from row 3 and end to row 5
+    // to check diagonal we have to decrease the row by 1 and increase the column by1
     for (var k = 3; k <= 5; k++) {
 
         var i = k;
@@ -146,7 +150,7 @@ function checkRedIsWinner() {
             } else {
                 count = 0;
             }
-            i--;
+            i--;  // decrease the row by 1 to go diagonally up
             if (count == 4) {
                 announceWinner("red");
                 return;
@@ -156,6 +160,7 @@ function checkRedIsWinner() {
     }
 
     // Daigonal Checking for winner in all possible cases starting last row, 2nd column
+    // Here we start from column 1 and end to 3
     for (var k = 0; k < 3; k++) {
         var i = 5;
         var count = 0;
@@ -182,8 +187,8 @@ function checkBlackIsWinner() {
     //Vertical Winner Check for black
     for (var i = 0; i < 7; i++) {
         var verticalCount = 0;
-        for (var k = 0; k < 6; k++) {
-            if (color[k][i] == "b") {
+        for (var j = 0; j < 6; j++) {
+            if (color[j][i] == "b") {
                 verticalCount++;
             } else {
                 verticalCount = 0;
@@ -257,7 +262,7 @@ function checkBlackIsWinner() {
 
 }
 
-
+// This will annnounce the winner alert and wait for 1.2 sec to relad the game again.
 function announceWinner(color) {
     alert(color + " is winner");
     setTimeout(function() {
